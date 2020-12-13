426501272c5ec7f8f864302cf5cdd7cc44df5fe4
KurisuSeima <kurisuseima@gmail.com>
2020-12-13 20:52:48 +0900

    初期コミット

---
+++ CollectionApp/CollectionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionApp
+//
+//  Created by くりすせいま on 2020/12/13.
+//  Copyright © 2020 星舞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
